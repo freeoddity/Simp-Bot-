9e50a2dd9c6ded684793429804e23bb39a3d78c5
freeoddity <73929760+freeoddity@users.noreply.github.com>
2021-08-02 21:13:07 -0400

    Add files via upload

---
+++ Simp.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+
+public class Simp {
+	public static JDA jda;
+	public static String prefix = "--";
+	public static void main (String[] args) throws Exception
+	{
+		jda = JDABuilder.createDefault("Your Token Here").build();
+		jda.addEventListener(new Commands());
+		jda.awaitReady();
+		List <Guild> Servers = jda.getGuilds();
+		jda.getPresence().setActivity(Activity.playing("Currently Serving " + Servers.size() + " Servers"));
+	}
+	
+	
+}
